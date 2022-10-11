184206f273b830b17177530da2348e1d9f1d3f24
Aitijya Sarkar <aitijya.sarkar95@gmail.com>
2022-10-11 16:17:49 +0530

    project 1 completed

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+ 
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  // this sets the state data
+  // which is then passed in as a prop using people
+
+  return (
+    <main>
+        <section className='container'>
+            <h3>{people.length} birthdays today</h3>
+            <List people={people} />
+            <button onClick={() => setPeople([])}>clear all</button>
+        </section>
+    </main>
+  )
+  // this is setting the main card
 }
 
 export default App;
